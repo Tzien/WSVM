d0bacd921d69563f35acc91871fac9d13efb7c13
zien.tian <15930816859@163.com>
2026-06-23 09:28:54 +0800

    新增项目

---
+++ lowcodeSubsevices/src/views/pictable/helper/columnList.ts
@@ -0,0 +1,117 @@
+﻿const columnList = [
+  {
+    "label": "图片上传",
+    "labelI18nCode": "",
+    "prop": "PicURL",
+    "fixed": "none",
+    "align": "left",
+    "ceriKey": "uploadImg",
+    "sortable": false,
+    "resizable": true,
+    "width": null,
+    "id": "PicURL",
+    "fullName": "图片上传",
+    "fullNameI18nCode": [
+      ""
+    ],
+    "__config__": {
+      "ceriKey": "uploadImg",
+      "label": "图片上传",
+      "tipLabel": "",
+      "showLabel": true,
+      "tag": "CeriUploadImg",
+      "tagIcon": "icon-ym icon-ym-generator-upload",
+      "tableAlign": "left",
+      "tableFixed": "none",
+      "className": [],
+      "defaultValue": [],
+      "required": false,
+      "layout": "colFormItem",
+      "span": 24,
+      "dragDisabled": false,
+      "visibility": [
+        "pc",
+        "app"
+      ],
+      "tableName": "pictable",
+      "noShow": false,
+      "regList": [],
+      "trigger": "change",
+      "formId": "formItem5a7b29",
+      "renderKey": 1782109155461
+    },
+    "on": {
+      "change": "({ data, rowIndex, formData, setFormData, setShowOrHide, setRequired, setDisabled, onlineUtils }) => {\n    // 在此编写代码\n    \n}"
+    },
+    "disabled": false,
+    "fileSize": 10,
+    "sizeUnit": "MB",
+    "buttonText": "点击上传",
+    "limit": 9,
+    "pathType": "defaultPath",
+    "sortRule": [],
+    "timeFormat": "YYYY",
+    "folder": "",
+    "tipText": "",
+    "showType": "card",
+    "__vModel__": "PicURL"
+  },
+  {
+    "label": "文件上传",
+    "labelI18nCode": "",
+    "prop": "FileURL",
+    "fixed": "none",
+    "align": "left",
+    "ceriKey": "uploadFile",
+    "sortable": false,
+    "resizable": true,
+    "width": null,
+    "id": "FileURL",
+    "fullName": "文件上传",
+    "fullNameI18nCode": [
+      ""
+    ],
+    "__config__": {
+      "ceriKey": "uploadFile",
+      "label": "文件上传",
+      "tipLabel": "",
+      "showLabel": true,
+      "tag": "CeriUploadFile",
+      "tagIcon": "icon-ym icon-ym-generator-upload",
+      "tableAlign": "left",
+      "tableFixed": "none",
+      "className": [],
+      "defaultValue": [],
+      "required": false,
+      "layout": "colFormItem",
+      "span": 24,
+      "dragDisabled": false,
+      "visibility": [
+        "pc",
+        "app"
+      ],
+      "tableName": "pictable",
+      "noShow": false,
+      "regList": [],
+      "trigger": "change",
+      "formId": "formItem422762",
+      "renderKey": 1782109156205
+    },
+    "on": {
+      "change": "({ data, rowIndex, formData, setFormData, setShowOrHide, setRequired, setDisabled, onlineUtils }) => {\n    // 在此编写代码\n    \n}"
+    },
+    "disabled": false,
+    "accept": "",
+    "fileSize": 10,
+    "sizeUnit": "MB",
+    "buttonText": "点击上传",
+    "limit": 9,
+    "pathType": "defaultPath",
+    "sortRule": [],
+    "timeFormat": "YYYY",
+    "folder": "",
+    "tipText": "",
+    "__vModel__": "FileURL"
+  }
+]
+export default columnList
