3aa5e44ea3de11317bd9926853104f1afd16edd1
zien.tian <15930816859@163.com>
2026-07-17 09:19:31 +0800

    新增文件

---
+++ lowcodeServices/Application/Interfaces/II18NtagService.cs
@@ -0,0 +1,8 @@
+﻿namespace CeriOS.示例.Interfaces;
+
+/// <summary>
+/// 业务抽象：测试其他模板.
+/// </summary>
+public interface II18NtagService
+{
+}
---
+++ lowcodeServices/Domain/Dto/I18Ntag/I18NtagInfoOutput.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CeriOS.示例.Entitys.Dto.I18Ntag;
+ 
+/// <summary>
+/// 测试其他模板输出参数.
+/// </summary>
+public class I18NtagInfoOutput
+{
+    /// <summary>
+    /// .
+    /// </summary>
+    public string? id { get; set; }
+
+    /// <summary>
+    /// .
+    /// </summary>
+    public string? TagName { get; set; }
+
+    /// <summary>
+    /// 0 通用标签、各个菜单内的元素  1 系统/菜单标签 .
+    /// </summary>
+    public int? Code { get; set; }
+
+}
