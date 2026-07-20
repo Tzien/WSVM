9f1bc86edce84ce1e7cd349bb8967941b57c0e18
zien.tian <15930816859@163.com>
2026-07-20 17:30:51 +0800

    提交代码

---
+++ lowcodeServices/Domain/Dto/Allprops/AllpropsInlineEditorOutput.cs
@@ -0,0 +1,53 @@
+﻿﻿namespace CeriOS.示例.Entitys.Dto.Allprops;
+ 
+/// <summary>
+/// 测试其他模板2输出参数.
+/// </summary>
+public class AllpropsInlineEditorOutput
+{
+    /// <summary>
+    /// .
+    /// </summary>
+    public int Enabled { get; set; }
+
+    /// <summary>
+    /// .
+    /// </summary>
+    public string Enabled_name { get; set; }
+
+    /// <summary>
+    /// .
+    /// </summary>
+    public string? id { get; set; }
+
+    /// <summary>
+    /// .
+    /// </summary>
+    public string? Name { get; set; }
+
+    /// <summary>
+    /// .
+    /// </summary>
+    public string? Name_name { get; set; }
+
+    /// <summary>
+    /// .
+    /// </summary>
+    public string? Remark { get; set; }
+
+    /// <summary>
+    /// .
+    /// </summary>
+    public string? Remark_name { get; set; }
+
+    /// <summary>
+    /// .
+    /// </summary>
+    public int? Sort { get; set; }
+
+    /// <summary>
+    /// .
+    /// </summary>
+    public int? Sort_name { get; set; }
+
+}
