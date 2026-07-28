d6be983c0a650103d66fa2d267aafdfc572902ad
zien.tian <15930816859@163.com>
2026-07-28 16:48:50 +0800

    新增项目

---
+++ lowcodeSubsevices/src/views/allprops2/helper/columnList.ts
@@ -0,0 +1,780 @@
+﻿const columnList = [
+  {
+    "label": "单行输入",
+    "labelI18nCode": "",
+    "prop": "DanHang",
+    "fixed": "none",
+    "align": "left",
+    "ceriKey": "input",
+    "sortable": false,
+    "resizable": true,
+    "width": null,
+    "id": "DanHang",
+    "fullName": "单行输入",
+    "fullNameI18nCode": [
+      ""
+    ],
+    "__config__": {
+      "ceriKey": "input",
+      "label": "单行输入",
+      "tipLabel": "",
+      "showLabel": true,
+      "tag": "CeriInput",
+      "tagIcon": "icon-ym icon-ym-generator-input",
+      "tableAlign": "left",
+      "tableFixed": "none",
+      "className": [],
+      "required": false,
+      "layout": "colFormItem",
+      "span": 24,
+      "dragDisabled": false,
+      "visibility": [
+        "pc",
+        "app"
+      ],
+      "tableName": "allprops2",
+      "noShow": false,
+      "regList": [],
+      "trigger": "blur",
+      "formId": "formItemf27ac9",
+      "renderKey": 1785227831650
+    },
+    "on": {
+      "change": "({ data, rowIndex, formData, setFormData, setShowOrHide, setRequired, setDisabled, onlineUtils }) => {\n    // 在此编写代码\n    \n}",
+      "blur": "({ data, rowIndex, formData, setFormData, setShowOrHide, setRequired, setDisabled, onlineUtils }) => {\n    // 在此编写代码\n    \n}"
+    },
+    "style": {
+      "width": "100%"
+    },
+    "placeholder": "请输入",
+    "useScan": false,
+    "useMask": false,
+    "maskConfig": {
+      "filler": "*",
+      "maskType": 1,
+      "prefixType": 1,
+      "prefixLimit": 0,
+      "prefixSpecifyChar": "",
+      "suffixType": 1,
+      "suffixLimit": 0,
+      "suffixSpecifyChar": "",
+      "ignoreChar": "",
+      "useUnrealMask": false,
+      "unrealMaskLength": 1
+    },
+    "clearable": true,
+    "addonBefore": "",
+    "addonAfter": "",
+    "prefixIcon": "",
+    "suffixIcon": "",
+    "maxlength": null,
+    "showCount": false,
+    "showPassword": false,
+    "readonly": false,
+    "disabled": false,
+    "__vModel__": "DanHang"
+  },
+  {
+    "label": "多行输入",
+    "labelI18nCode": "",
+    "prop": "DuoHang",
+    "fixed": "none",
+    "align": "left",
+    "ceriKey": "textarea",
+    "sortable": false,
+    "resizable": true,
+    "width": null,
+    "id": "DuoHang",
+    "fullName": "多行输入",
+    "fullNameI18nCode": [
+      ""
+    ],
+    "__config__": {
+      "ceriKey": "textarea",
+      "label": "多行输入",
+      "tipLabel": "",
+      "showLabel": true,
+      "tag": "CeriTextarea",
+      "tagIcon": "icon-ym icon-ym-generator-textarea",
+      "tableAlign": "left",
+      "tableFixed": "none",
+      "className": [],
+      "required": false,
+      "layout": "colFormItem",
+      "span": 24,
+      "dragDisabled": false,
+      "visibility": [
+        "pc",
+        "app"
+      ],
+      "tableName": "allprops2",
+      "noShow": false,
+      "regList": [],
+      "trigger": "blur",
+      "formId": "formItem4e2b5c",
+      "renderKey": 1785227838152
+    },
+    "on": {
+      "change": "({ data, rowIndex, formData, setFormData, setShowOrHide, setRequired, setDisabled, onlineUtils }) => {\n    // 在此编写代码\n    \n}",
+      "blur": "({ data, rowIndex, formData, setFormData, setShowOrHide, setRequired, setDisabled, onlineUtils }) => {\n    // 在此编写代码\n    \n}"
+    },
+    "style": {
+      "width": "100%"
+    },
+    "placeholder": "请输入",
+    "autoSize": {
+      "minRows": 4,
+      "maxRows": 4
+    },
+    "clearable": true,
+    "maxlength": null,
+    "showCount": false,
+    "readonly": false,
+    "disabled": false,
+    "__vModel__": "DuoHang"
+  },
+  {
+    "label": "数字输入",
+    "labelI18nCode": "",
+    "prop": "ShuZi",
+    "fixed": "none",
+    "align": "left",
+    "ceriKey": "inputNumber",
+    "sortable": false,
+    "resizable": true,
+    "width": null,
+    "id": "ShuZi",
+    "fullName": "数字输入",
+    "fullNameI18nCode": [
+      ""
+    ],
+    "__config__": {
+      "ceriKey": "inputNumber",
+      "label": "数字输入",
+      "tipLabel": "",
+      "showLabel": true,
+      "tag": "CeriInputNumber",
+      "tagIcon": "icon-ym icon-ym-generator-number",
+      "tableAlign": "left",
+      "tableFixed": "none",
+      "className": [],
+      "required": false,
+      "layout": "colFormItem",
+      "span": 24,
+      "dragDisabled": false,
+      "visibility": [
+        "pc",
+        "app"
+      ],
+      "tableName": "allprops2",
+      "noShow": false,
+      "regList": [],
+      "trigger": [
+        "blur",
+        "change"
+      ],
+      "formId": "formItem76ae46",
+      "renderKey": 1785227839002
+    },
+    "on": {
+      "change": "({ data, rowIndex, formData, setFormData, setShowOrHide, setRequired, setDisabled, onlineUtils }) => {\n    // 在此编写代码\n    \n}",
+      "blur": "({ data, rowIndex, formData, setFormData, setShowOrHide, setRequired, setDisabled, onlineUtils }) => {\n    // 在此编写代码\n    \n}"
+    },
+    "style": {
+      "width": "100%"
+    },
+    "placeholder": "请输入",
+    "controls": false,
+    "addonBefore": "",
+    "addonAfter": "",
+    "thousands": false,
+    "isAmountChinese": false,
+    "step": 1,
+    "disabled": false,
+    "__vModel__": "ShuZi"
+  },
+  {
+    "label": "开关",
+    "labelI18nCode": "",
+    "prop": "KaiGuan",
+    "fixed": "none",
+    "align": "left",
+    "ceriKey": "switch",
+    "sortable": false,
+    "resizable": true,
+    "width": null,
+    "id": "KaiGuan",
+    "fullName": "开关",
+    "fullNameI18nCode": [
+      ""
+    ],
+    "__config__": {
+      "ceriKey": "switch",
+      "label": "开关",
+      "tipLabel": "",
+      "showLabel": true,
+      "tag": "CeriSwitch",
+      "tagIcon": "icon-ym icon-ym-generator-switch",
+      "tableAlign": "left",
+      "tableFixed": "none",
+      "className": [],
+      "defaultValue": 0,
+      "required": false,
+      "layout": "colFormItem",
+      "span": 24,
+      "dragDisabled": false,
+      "visibility": [
+        "pc",
+        "app"
+      ],
+      "tableName": "allprops2",
+      "noShow": false,
+      "regList": [],
+      "trigger": "change",
+      "formId": "formItem5e2c89",
+      "renderKey": 1785227839448
+    },
+    "on": {
+      "change": "({ data, rowIndex, formData, setFormData, setShowOrHide, setRequired, setDisabled, onlineUtils }) => {\n    // 在此编写代码\n    \n}"
+    },
+    "disabled": false,
+    "activeTxt": "开",
+    "inactiveTxt": "关",
+    "activeValue": 1,
+    "inactiveValue": 0,
+    "__vModel__": "KaiGuan"
+  },
+  {
+    "label": "单选框组",
+    "labelI18nCode": "",
+    "prop": "DanXuan",
+    "fixed": "none",
+    "align": "left",
+    "ceriKey": "radio",
+    "sortable": false,
+    "resizable": true,
+    "width": null,
+    "id": "DanXuan",
+    "fullName": "单选框组",
+    "fullNameI18nCode": [
+      ""
+    ],
+    "__config__": {
+      "ceriKey": "radio",
+      "label": "单选框组",
+      "tipLabel": "",
+      "showLabel": true,
+      "tag": "CeriRadio",
+      "tagIcon": "icon-ym icon-ym-generator-radio",
+      "tableAlign": "left",
+      "tableFixed": "none",
+      "className": [],
+      "required": false,
+      "layout": "colFormItem",
+      "span": 24,
+      "dragDisabled": false,
+      "visibility": [
+        "pc",
+        "app"
+      ],
+      "tableName": "allprops2",
+      "noShow": false,
+      "regList": [],
+      "trigger": "change",
+      "dataType": "static",
+      "dictionaryType": "",
+      "propsUrl": "",
+      "propsName": "",
+      "useCache": true,
+      "templateJson": [],
+      "formId": "formItem986ed6",
+      "renderKey": 1785227839888
+    },
+    "on": {
+      "change": "({ data, rowIndex, formData, setFormData, setShowOrHide, setRequired, setDisabled, onlineUtils }) => {\n    // 在此编写代码\n    \n}"
+    },
+    "style": {
+      "width": "100%"
+    },
+    "options": [
+      {
+        "fullName": "选项一",
+        "id": "1"
+      },
+      {
+        "fullName": "选项二",
+        "id": "2"
+      }
+    ],
+    "props": {
+      "label": "fullName",
+      "value": "id"
+    },
+    "direction": "horizontal",
+    "optionType": "default",
+    "buttonStyle": "solid",
+    "size": "default",
+    "disabled": false,
+    "__vModel__": "DanXuan"
+  },
+  {
+    "label": "多选框组",
+    "labelI18nCode": "",
+    "prop": "DuoXuan",
+    "fixed": "none",
+    "align": "left",
+    "ceriKey": "checkbox",
+    "sortable": false,
+    "resizable": true,
+    "width": null,
+    "id": "DuoXuan",
+    "fullName": "多选框组",
+    "fullNameI18nCode": [
+      ""
+    ],
+    "__config__": {
+      "ceriKey": "checkbox",
+      "label": "多选框组",
+      "tipLabel": "",
+      "showLabel": true,
+      "tag": "CeriCheckbox",
+      "tagIcon": "icon-ym icon-ym-generator-checkbox",
+      "tableAlign": "left",
+      "tableFixed": "none",
+      "className": [],
+      "defaultValue": [],
+      "required": false,
+      "layout": "colFormItem",
+      "span": 24,
+      "dragDisabled": false,
+      "visibility": [
+        "pc",
+        "app"
+      ],
+      "tableName": "allprops2",
+      "noShow": false,
+      "regList": [],
+      "trigger": "change",
+      "dataType": "static",
+      "dictionaryType": "",
+      "propsUrl": "",
+      "propsName": "",
+      "useCache": true,
+      "templateJson": [],
+      "formId": "formIteme33f48",
+      "renderKey": 1785227840361
+    },
+    "on": {
+      "change": "({ data, rowIndex, formData, setFormData, setShowOrHide, setRequired, setDisabled, onlineUtils }) => {\n    // 在此编写代码\n    \n}"
+    },
+    "style": {
+      "width": "100%"
+    },
+    "options": [
+      {
+        "fullName": "选项一",
+        "id": "1"
+      },
+      {
+        "fullName": "选项二",
+        "id": "2"
+      }
+    ],
+    "props": {
+      "label": "fullName",
+      "value": "id"
+    },
+    "direction": "horizontal",
+    "disabled": false,
+    "__vModel__": "DuoXuan"
+  },
+  {
+    "label": "下拉选择",
+    "labelI18nCode": "",
+    "prop": "XiaLa",
+    "fixed": "none",
+    "align": "left",
+    "ceriKey": "select",
+    "sortable": false,
+    "resizable": true,
+    "width": null,
+    "id": "XiaLa",
+    "fullName": "下拉选择",
+    "fullNameI18nCode": [
+      ""
+    ],
+    "__config__": {
+      "ceriKey": "select",
+      "label": "下拉选择",
+      "tipLabel": "",
+      "showLabel": true,
+      "tag": "CeriSelect",
+      "tagIcon": "icon-ym icon-ym-generator-select",
+      "tableAlign": "left",
+      "tableFixed": "none",
+      "className": [],
+      "defaultValue": "",
+      "required": false,
+      "layout": "colFormItem",
+      "span": 24,
+      "dragDisabled": false,
+      "visibility": [
+        "pc",
+        "app"
+      ],
+      "tableName": "allprops2",
+      "noShow": false,
+      "regList": [],
+      "trigger": "change",
+      "dataType": "static",
+      "dictionaryType": "",
+      "propsUrl": "",
+      "propsName": "",
+      "useCache": true,
+      "templateJson": [],
+      "formId": "formItem9fe655",
+      "renderKey": 1785227840801
+    },
+    "on": {
+      "change": "({ data, rowIndex, formData, setFormData, setShowOrHide, setRequired, setDisabled, onlineUtils }) => {\n    // 在此编写代码\n    \n}"
+    },
+    "style": {
+      "width": "100%"
+    },
+    "options": [
+      {
+        "fullName": "选项一",
+        "id": "1"
+      },
+      {
+        "fullName": "选项二",
+        "id": "2"
+      }
+    ],
+    "props": {
+      "label": "fullName",
+      "value": "id"
+    },
+    "placeholder": "请选择",
+    "clearable": true,
+    "disabled": false,
+    "filterable": false,
+    "multiple": false,
+    "__vModel__": "XiaLa"
+  },
+  {
+    "label": "级联选择",
+    "labelI18nCode": "",
+    "prop": "JiLian",
+    "fixed": "none",
+    "align": "left",
+    "ceriKey": "cascader",
+    "sortable": false,
+    "resizable": true,
+    "width": null,
+    "id": "JiLian",
+    "fullName": "级联选择",
+    "fullNameI18nCode": [
+      ""
+    ],
+    "__config__": {
+      "ceriKey": "cascader",
+      "label": "级联选择",
+      "tipLabel": "",
+      "showLabel": true,
+      "tag": "CeriCascader",
+      "tagIcon": "icon-ym icon-ym-generator-cascader",
+      "tableAlign": "left",
+      "tableFixed": "none",
+      "className": [],
+      "defaultValue": [],
+      "required": false,
+      "layout": "colFormItem",
+      "span": 24,
+      "dragDisabled": false,
+      "visibility": [
+        "pc",
+        "app"
+      ],
+      "tableName": "allprops2",
+      "noShow": false,
+      "regList": [],
+      "trigger": "change",
+      "dataType": "static",
+      "propsUrl": "",
+      "propsName": "",
+      "useCache": true,
+      "templateJson": [],
+      "dictionaryType": "",
+      "formId": "formItem18744e",
+      "renderKey": 1785227841360
+    },
+    "on": {
+      "change": "({ data, rowIndex, formData, setFormData, setShowOrHide, setRequired, setDisabled, onlineUtils }) => {\n    // 在此编写代码\n    \n}",
+      "blur": "({ data, rowIndex, formData, setFormData, setShowOrHide, setRequired, setDisabled, onlineUtils }) => {\n    // 在此编写代码\n    \n}"
+    },
+    "style": {
+      "width": "100%"
+    },
+    "options": [
+      {
+        "id": "1",
+        "fullName": "选项1",
+        "children": [
+          {
+            "id": "2",
+            "fullName": "选项1-1"
+          }
+        ]
+      }
+    ],
+    "props": {
+      "value": "id",
+      "label": "fullName",
+      "children": "children"
+    },
+    "placeholder": "请选择",
+    "disabled": false,
+    "clearable": true,
+    "filterable": false,
+    "multiple": false,
+    "__vModel__": "JiLian"
+  },
+  {
+    "label": "日期选择",
+    "labelI18nCode": "",
+    "prop": "RiQi",
+    "fixed": "none",
+    "align": "left",
+    "ceriKey": "datePicker",
+    "sortable": false,
+    "resizable": true,
+    "width": null,
+    "id": "RiQi",
+    "fullName": "日期选择",
+    "fullNameI18nCode": [
+      ""
+    ],
+    "__config__": {
+      "ceriKey": "datePicker",
+      "label": "日期选择",
+      "tipLabel": "",
+      "showLabel": true,
+      "tag": "CeriDatePicker",
+      "tagIcon": "icon-ym icon-ym-generator-date",
+      "tableAlign": "left",
+      "tableFixed": "none",
+      "className": [],
+      "defaultValue": null,
+      "defaultCurrent": false,
+      "required": false,
+      "layout": "colFormItem",
+      "span": 24,
+      "dragDisabled": false,
+      "visibility": [
+        "pc",
+        "app"
+      ],
+      "tableName": "allprops2",
+      "noShow": false,
+      "regList": [],
+      "trigger": "change",
+      "startTimeRule": false,
+      "startTimeType": 1,
+      "startTimeTarget": 1,
+      "startTimeValue": null,
+      "startRelationField": "",
+      "endTimeRule": false,
+      "endTimeType": 1,
+      "endTimeTarget": 1,
+      "endTimeValue": null,
+      "endRelationField": "",
+      "formId": "formItemae6aad",
+      "renderKey": 1785227841760
+    },
+    "on": {
+      "change": "({ data, rowIndex, formData, setFormData, setShowOrHide, setRequired, setDisabled, onlineUtils }) => {\n    // 在此编写代码\n    \n}",
+      "blur": "({ data, rowIndex, formData, setFormData, setShowOrHide, setRequired, setDisabled, onlineUtils }) => {\n    // 在此编写代码\n    \n}"
+    },
+    "style": {
+      "width": "100%"
+    },
+    "placeholder": "请选择",
+    "format": "yyyy-MM-dd",
+    "startTime": null,
+    "endTime": null,
+    "disabled": false,
+    "clearable": true,
+    "__vModel__": "RiQi"
+  },
+  {
+    "label": "时间选择",
+    "labelI18nCode": "",
+    "prop": "ShiJian",
+    "fixed": "none",
+    "align": "left",
+    "ceriKey": "timePicker",
+    "sortable": false,
+    "resizable": true,
+    "width": null,
+    "id": "ShiJian",
+    "fullName": "时间选择",
+    "fullNameI18nCode": [
+      ""
+    ],
+    "__config__": {
+      "ceriKey": "timePicker",
+      "label": "时间选择",
+      "tipLabel": "",
+      "showLabel": true,
+      "tag": "CeriTimePicker",
+      "tagIcon": "icon-ym icon-ym-generator-time",
+      "tableAlign": "left",
+      "tableFixed": "none",
+      "className": [],
+      "defaultValue": null,
+      "defaultCurrent": false,
+      "required": false,
+      "layout": "colFormItem",
+      "span": 24,
+      "dragDisabled": false,
+      "visibility": [
+        "pc",
+        "app"
+      ],
+      "tableName": "allprops2",
+      "noShow": false,
+      "regList": [],
+      "trigger": "change",
+      "startTimeRule": false,
+      "startTimeType": 1,
+      "startTimeTarget": 1,
+      "startTimeValue": null,
+      "startRelationField": "",
+      "endTimeRule": false,
+      "endTimeType": 1,
+      "endTimeTarget": 1,
+      "endTimeValue": null,
+      "endRelationField": "",
+      "formId": "formIteme93014",
+      "renderKey": 1785227842136
+    },
+    "on": {
+      "change": "({ data, rowIndex, formData, setFormData, setShowOrHide, setRequired, setDisabled, onlineUtils }) => {\n    // 在此编写代码\n    \n}",
+      "blur": "({ data, rowIndex, formData, setFormData, setShowOrHide, setRequired, setDisabled, onlineUtils }) => {\n    // 在此编写代码\n    \n}"
+    },
+    "style": {
+      "width": "100%"
+    },
+    "placeholder": "请选择",
+    "format": "HH:mm:ss",
+    "startTime": null,
+    "endTime": null,
+    "disabled": false,
+    "clearable": true,
+    "__vModel__": "ShiJian"
+  },
+  {
+    "label": "评分",
+    "labelI18nCode": "",
+    "prop": "PF",
+    "fixed": "none",
+    "align": "left",
+    "ceriKey": "rate",
+    "sortable": false,
+    "resizable": true,
+    "width": null,
+    "id": "PF",
+    "fullName": "评分",
+    "fullNameI18nCode": [
+      ""
+    ],
+    "__config__": {
+      "ceriKey": "rate",
+      "label": "评分",
+      "tipLabel": "",
+      "showLabel": true,
+      "tag": "CeriRate",
+      "tagIcon": "icon-ym icon-ym-generator-rate",
+      "tableAlign": "left",
+      "tableFixed": "none",
+      "className": [],
+      "defaultValue": 0,
+      "required": false,
+      "layout": "colFormItem",
+      "span": 24,
+      "dragDisabled": false,
+      "visibility": [
+        "pc",
+        "app"
+      ],
+      "tableName": "allprops2",
+      "noShow": false,
+      "regList": [],
+      "trigger": "change",
+      "formId": "formItem4175a0",
+      "renderKey": 1785227842992
+    },
+    "on": {
+      "change": "({ data, rowIndex, formData, setFormData, setShowOrHide, setRequired, setDisabled, onlineUtils }) => {\n    // 在此编写代码\n    \n}"
+    },
+    "count": 5,
+    "allowHalf": false,
+    "disabled": false,
+    "__vModel__": "PF"
+  },
+  {
+    "label": "滑块",
+    "labelI18nCode": "",
+    "prop": "HK",
+    "fixed": "none",
+    "align": "left",
+    "ceriKey": "slider",
+    "sortable": false,
+    "resizable": true,
+    "width": null,
+    "id": "HK",
+    "fullName": "滑块",
+    "fullNameI18nCode": [
+      ""
+    ],
+    "__config__": {
+      "ceriKey": "slider",
+      "label": "滑块",
+      "tipLabel": "",
+      "showLabel": true,
+      "tag": "CeriSlider",
+      "tagIcon": "icon-ym icon-ym-generator-slider",
+      "tableAlign": "left",
+      "tableFixed": "none",
+      "className": [],
+      "defaultValue": 0,
+      "required": false,
+      "layout": "colFormItem",
+      "span": 24,
+      "dragDisabled": false,
+      "visibility": [
+        "pc",
+        "app"
+      ],
+      "tableName": "allprops2",
+      "noShow": false,
+      "regList": [],
+      "trigger": "change",
+      "formId": "formItem656b92",
+      "renderKey": 1785227843433
+    },
+    "on": {
+      "change": "({ data, rowIndex, formData, setFormData, setShowOrHide, setRequired, setDisabled, onlineUtils }) => {\n    // 在此编写代码\n    \n}"
+    },
+    "style": {
+      "width": "100%"
+    },
+    "disabled": false,
+    "min": 0,
+    "max": 100,
+    "step": 1,
+    "__vModel__": "HK"
+  }
+]
+export default columnList
---
+++ lowcodeSubsevices/src/views/allprops2/helper/searchList.ts
@@ -0,0 +1,133 @@
+﻿const searchList = [
+  {
+    "label": "单行输入",
+    "labelI18nCode": "",
+    "prop": "DanHang",
+    "ceriKey": "input",
+    "searchType": 2,
+    "searchMultiple": false,
+    "isKeyword": false,
+    "id": "DanHang",
+    "fullName": "单行输入",
+    "fullNameI18nCode": [
+      ""
+    ],
+    "__config__": {
+      "ceriKey": "input",
+      "label": "单行输入",
+      "tipLabel": "",
+      "showLabel": true,
+      "tag": "CeriInput",
+      "tagIcon": "icon-ym icon-ym-generator-input",
+      "tableAlign": "left",
+      "tableFixed": "none",
+      "className": [],
+      "required": false,
+      "layout": "colFormItem",
+      "span": 24,
+      "dragDisabled": false,
+      "visibility": [
+        "pc",
+        "app"
+      ],
+      "tableName": "allprops2",
+      "noShow": false,
+      "regList": [],
+      "trigger": "blur",
+      "formId": "formItemf27ac9",
+      "renderKey": 1785227831650
+    },
+    "on": {
+      "change": "({ data, rowIndex, formData, setFormData, setShowOrHide, setRequired, setDisabled, onlineUtils }) => {\n    // 在此编写代码\n    \n}",
+      "blur": "({ data, rowIndex, formData, setFormData, setShowOrHide, setRequired, setDisabled, onlineUtils }) => {\n    // 在此编写代码\n    \n}"
+    },
+    "style": {
+      "width": "100%"
+    },
+    "placeholder": "请输入",
+    "useScan": false,
+    "useMask": false,
+    "maskConfig": {
+      "filler": "*",
+      "maskType": 1,
+      "prefixType": 1,
+      "prefixLimit": 0,
+      "prefixSpecifyChar": "",
+      "suffixType": 1,
+      "suffixLimit": 0,
+      "suffixSpecifyChar": "",
+      "ignoreChar": "",
+      "useUnrealMask": false,
+      "unrealMaskLength": 1
+    },
+    "clearable": true,
+    "addonBefore": "",
+    "addonAfter": "",
+    "prefixIcon": "",
+    "suffixIcon": "",
+    "maxlength": null,
+    "showCount": false,
+    "showPassword": false,
+    "readonly": false,
+    "disabled": false,
+    "__vModel__": "DanHang"
+  },
+  {
+    "label": "多行输入",
+    "labelI18nCode": "",
+    "prop": "DuoHang",
+    "ceriKey": "textarea",
+    "searchType": 2,
+    "searchMultiple": false,
+    "isKeyword": false,
+    "id": "DuoHang",
+    "fullName": "多行输入",
+    "fullNameI18nCode": [
+      ""
+    ],
+    "__config__": {
+      "ceriKey": "textarea",
+      "label": "多行输入",
+      "tipLabel": "",
+      "showLabel": true,
+      "tag": "CeriTextarea",
+      "tagIcon": "icon-ym icon-ym-generator-textarea",
+      "tableAlign": "left",
+      "tableFixed": "none",
+      "className": [],
+      "required": false,
+      "layout": "colFormItem",
+      "span": 24,
+      "dragDisabled": false,
+      "visibility": [
+        "pc",
+        "app"
+      ],
+      "tableName": "allprops2",
+      "noShow": false,
+      "regList": [],
+      "trigger": "blur",
+      "formId": "formItem4e2b5c",
+      "renderKey": 1785227838152
+    },
+    "on": {
+      "change": "({ data, rowIndex, formData, setFormData, setShowOrHide, setRequired, setDisabled, onlineUtils }) => {\n    // 在此编写代码\n    \n}",
+      "blur": "({ data, rowIndex, formData, setFormData, setShowOrHide, setRequired, setDisabled, onlineUtils }) => {\n    // 在此编写代码\n    \n}"
+    },
+    "style": {
+      "width": "100%"
+    },
+    "placeholder": "请输入",
+    "autoSize": {
+      "minRows": 4,
+      "maxRows": 4
+    },
+    "clearable": true,
+    "maxlength": null,
+    "showCount": false,
+    "readonly": false,
+    "disabled": false,
+    "__vModel__": "DuoHang"
+  }
+]
+export default searchList
